e53c0f2e0150095b015a7cb3d42edc3b3e7417aa
EpicFn <rlagurwls056@gmail.com>
2025-08-04 21:02:23 +0900

    design : 사이드 메뉴 구성

---
+++ frontend/src/constants/colors.tsx
@@ -0,0 +1,16 @@
+export const COLOR_BROWN = "#57564F";
+export const COLOR_GRAY = "#7A7A73";
+export const COLOR_BEIGE = "#DDDAD0";
+export const COLOR_YELLOW = "#F8F3CE";
+export const COLOR_WHITE = "#FFFFFF";
+export const COLOR_BLACK = "#000000";
+
+// 색상들을 객체로 묶어서 export 할 수도 있습니다.
+export const COLORS = {
+    brown: COLOR_BROWN,
+    gray: COLOR_GRAY,
+    beige: COLOR_BEIGE,
+    yellow: COLOR_YELLOW,
+    white: COLOR_WHITE,
+    black: COLOR_BLACK
+};
