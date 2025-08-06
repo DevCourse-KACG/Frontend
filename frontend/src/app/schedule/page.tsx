41d3a5315dd50c19b091e8392d5d16accd7b5b7b
sso0om <ssooom142@gmail.com>
2025-08-06 10:39:18 +0900

    refactor: 코드래빗 반영

--- frontend/src/app/schedule/page.tsx
+++ frontend/src/app/schedule/page.tsx
@@ -20,6 +20,10 @@ export default function ScheduleListPage() {
   // 파라미터 처리 - 모임 아이디
   const parm = useParams();
   const clubId = Number(parm.clubId);
+ 
+  if (isNaN(clubId)) {
+    throw new Error('유효하지 않은 모임입니다.');
+  }
 
   // 캘린더 처리
   const calendarRef = useRef<FullCalendar>(null);
