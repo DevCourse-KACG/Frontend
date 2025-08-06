249d9d50c984d4fdd1d03fd6ea1252dfcd16d5d7
sso0om <ssooom142@gmail.com>
2025-08-06 10:41:58 +0900

    [fix] 일정에서 클럽 id 숫자 처리 (#68)
    * fix: 클럽 id 숫자로
    
    * refactor: 코드래빗 반영

--- frontend/src/app/schedule/page.tsx
+++ frontend/src/app/schedule/page.tsx
@@ -19,7 +19,11 @@ import '@/lib/fullcalendar.css';
 export default function ScheduleListPage() {
   // 파라미터 처리 - 모임 아이디
   const parm = useParams();
-  const clubId = parm.clubId;
+  const clubId = Number(parm.clubId);
+ 
+  if (isNaN(clubId)) {
+    throw new Error('유효하지 않은 모임입니다.');
+  }
 
   // 캘린더 처리
   const calendarRef = useRef<FullCalendar>(null);
